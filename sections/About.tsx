6e0d7084746f7b75656056482949f0299acc480d
rsoylu <rana@soylu.org>
2024-07-14 02:59:04 -0500

    fsffsddfad

--- sections/About.tsx
+++ sections/About.tsx
@@ -1,13 +1,12 @@
 export default function Section() {
   return (
-    <div>
-      <div class="text-6xl bg-secondary"> A HAIKU</div>
+    <div class=" absolute bg-secondary w-screen pt-8 pb-18 z-50 flex flex-col text-center gap-y-15 h-18">
+      <div class="text-6xl text-white"> A HAIKU</div>
       <div>
-        <p>Beneath the night sky,</p>
-        <p>A sailor's call drifts afar,</p>
-        <p>Stars guide his message.</p>
+        <p class="text-white text-xl">Beneath the night sky,</p>
+        <p class="text-white text-xl">A sailor's call drifts afar,</p>
+        <p class="text-white text-xl">Stars guide his message.</p>
       </div>
     </div>
-    //HELLLLOOOO???
   )
 }
