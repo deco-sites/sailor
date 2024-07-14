d7bbdc159e61ca903c4ca18f1e01846771fff63a
rsoylu <rana@soylu.org>
2024-07-14 06:40:09 -0500

    themes

--- sections/About.tsx
+++ sections/About.tsx
@@ -2,13 +2,13 @@ export default function Section() {
   return (
     <div className="relative bg-secondary w-screen min-h-screen flex items-center justify-center py-24 px-4">
       <div className="text-center">
-        <div className="text-6xl text-white pb-10">A HAIKU</div>
+        <div className="text-6xl text-neutral pb-10">A HAIKU</div>
         <div>
-          <p className="text-white text-xl pb-6">Beneath the night sky,</p>
-          <p className="text-white text-xl pb-6">
+          <p className="text-neutral text-xl pb-6">Beneath the night sky,</p>
+          <p className="text-neutral text-xl pb-6">
             A sailor's call drifts afar,
           </p>
-          <p className="text-white text-xl pb-6">Stars guide his message.</p>
+          <p className="text-neutral text-xl pb-6">Stars guide his message.</p>
         </div>
       </div>
     </div>
