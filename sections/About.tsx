e5fcdee10771cc2b20afde0863a60e7ea7d1e4b6
rsoylu <rana@soylu.org>
2024-07-14 03:01:19 -0500

    gfdg

--- sections/About.tsx
+++ sections/About.tsx
@@ -1,6 +1,6 @@
 export default function Section() {
   return (
-    <div class=" absolute bg-secondary w-screen pt-8 pb-18 z-50 flex flex-col text-center gap-y-15 h-18">
+    <div class=" absolute bg-secondary w-screen pt-8 pb-18 z-50 flex-1 flex-col text-center gap-y-15 h-18">
       <div class="text-6xl text-white"> A HAIKU</div>
       <div>
         <p class="text-white text-xl">Beneath the night sky,</p>
